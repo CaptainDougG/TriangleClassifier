1a08aa930fe9f1551867f860fc8257b82eddb64c
isaiahgrills <isaiahgrills@10.0.0.166>
2021-03-15 18:10:53 -0600

    Finished TriangleClassifier

--- TriangleClassifier/src/TriangleClassifier.java
+++ TriangleClassifier/src/TriangleClassifier.java
@@ -35,26 +35,29 @@ public class TriangleClassifier {
 	}
 	
 	public static String getClassification(int aOne, int aTwo, int aThree) {
-		if ((aOne == 90 || aTwo == 90 || aThree ==90) && (aOne != aTwo && aOne != aThree)) {
+		if ((aOne + aTwo + aThree == 180) &&(aOne == 90 || aTwo == 90 || aThree ==90) && (aOne != aTwo && aOne != aThree && aTwo != aThree)) {
 			return "right scalene";
 		}
-		if ((aThree == aOne || aThree == aTwo || aOne == aTwo) && (aOne == 90 || aTwo == 90 || aThree ==90)) {
+		if ((aOne + aTwo + aThree == 180) && (aOne > 0 && aTwo > 0 && aThree > 0) && (aThree == aOne || aThree == aTwo || aOne == aTwo) && (aOne == 90 || aTwo == 90 || aThree ==90)) {
 			return "right isosceles";
 		}
-		if ((aThree == aOne || aThree == aTwo || aOne == aTwo) && (aOne > 90 || aTwo > 90 || aThree > 90)) {
+		if ((aOne + aTwo + aThree == 180) && (aOne > 0 && aTwo > 0 && aThree > 0) && (aThree == aOne || aThree == aTwo || aOne == aTwo) && (aOne > 90 || aTwo > 90 || aThree > 90)) {
 			return "obtuse isosceles";
 		}
-		if ((aOne != aTwo && aOne != aThree) && (aOne > 90 || aTwo > 90 || aThree > 90)) {
+		if ((aOne + aTwo + aThree == 180) && (aOne > 0 && aTwo > 0 && aThree > 0) && (aOne != aTwo && aOne != aThree) && (aOne > 90 || aTwo > 90 || aThree > 90)) {
 			return "obtuse scalene";
 		}
-		if (aOne == aTwo && aOne == aThree) {
+		if ((aOne + aTwo + aThree == 180) && (aOne > 0 && aTwo > 0 && aThree > 0) && (aOne == aTwo && aOne == aThree)) {
 			return "equilateral";
 		}
-		if ((aOne < 90 || aTwo < 90 || aThree < 90) && (aThree == aOne || aThree == aTwo || aOne == aTwo)) {
+		if ((aOne + aTwo + aThree == 180) && (aOne > 0 && aTwo > 0 && aThree > 0) && (aOne < 90 || aTwo < 90 || aThree < 90) && (aThree == aOne || aThree == aTwo || aOne == aTwo)) {
 			return "acute isosceles";
 		}
-		if ((aOne < 90 || aTwo < 90 || aThree < 90) && (aOne != aTwo && aOne != aThree)) {
+		if ((aOne + aTwo + aThree == 180) && (aOne > 0 && aTwo > 0 && aThree > 0) && (aOne < 90 || aTwo < 90 || aThree < 90) && (aOne != aTwo && aOne != aThree)) {
 			return "acute scalene";
+		}
+		else {
+			return "INVALID";
 		}
 	}
 }
